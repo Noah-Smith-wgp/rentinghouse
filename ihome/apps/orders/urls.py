cd5baf1492d5854f914d0ea6b55732c3db803b08
HeZhenfan <44939344+993007429@users.noreply.github.com>
2019-12-24 18:23:25 +0800

    订单模块

--- ihome/apps/orders/urls.py
+++ ihome/apps/orders/urls.py
@@ -1,5 +1,13 @@
 from django.conf.urls import url, include
 
+from apps.orders import views
+
 urlpatterns = [
+
+    url(r'^user/orders/$', views.OrderInfo.as_view({'get': 'list'})),
+    url(r'^orders/$', views.OrderInfo.as_view({'post': 'create'})),
+    url(r'^orders/(?P<pk>\d+)/status/$', views.OrderInfo.as_view({'put': 'accept_order'})),
+    url(r'^orders/(?P<pk>\d+)/comment/$', views.OrderInfo.as_view({'put': 'set_comment'})),
+
 
 ]
